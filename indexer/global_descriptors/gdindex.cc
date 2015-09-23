438b7da91dcb9d7b20ad450ddb6d186c59f4e255
Andre Araujo <andrefaraujo@gmail.com>
2015-09-23 14:01:22 -0700

    First version of GDIndex, only declarations and variables at this point

---
+++ indexer/global_descriptors/gdindex.cc
@@ -0,0 +1,155 @@
+#include <algorithm>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#include "../../common/feature_set/feature_set.h"
+#include "gdindex.h"
+
+extern "C" {
+#include "../../common/yael_v438_modif/yael/gmm.h"
+#include "../../common/yael_v438_modif/yael/matrix.h"
+}
+
+GDIndex::GDIndex() {
+    // Initializing member variables to default values
+    // -- Index
+    index_.number_global_descriptors = 0;
+    index_.word_descriptor.clear();
+    index_.word_l1_norms.clear();
+    index_.word_total_soft_assignment.clear();
+    index_.frame_numbers_in_db.clear();
+    // -- Index parameters
+    index_parameters_.ld_length = LD_LENGTH_DEFAULT;
+    index_parameters_.ld_frame_length = LD_FRAME_LENGTH_DEFAULT;
+    index_parameters_.ld_extension = LD_EXTENSION_DEFAULT;
+    index_parameters_.ld_name = LD_NAME_DEFAULT;
+    index_parameters_.ld_pca_dim = LD_PCA_DIM_DEFAULT;
+    index_parameters_.ld_pre_pca_power = LD_PRE_PCA_POWER_DEFAULT;
+    index_parameters_.ld_mean_vector = NULL;
+    index_parameters_.ld_pca_eigenvectors.clear();
+    index_parameters_.gd_gmm = NULL;
+    index_parameters_.gd_number_gaussians = GD_NUMBER_GAUSSIANS_DEFAULT;
+    index_parameters_.gd_power = GD_POWER_DEFAULT;
+    // -- Query parameters
+    query_parameters_.min_number_words_visited = MIN_NUMBER_WORDS_VISITED_DEFAULT;
+    query_parameters_.word_selection_mode = WORD_SELECTION_MODE_DEFAULT;
+    query_parameters_.word_selection_thresh = WORD_SELECTION_THRESH_DEFAULT;
+    query_parameters_.fast_corr_weights = NULL;
+}
+
+void GDIndex::write(const string index_path) {
+
+}
+
+void GDIndex::read(const string index_path) {
+
+}
+
+void GDIndex::write_frame_list(const string file_path) {
+    
+}
+
+void GDIndex::clean_index() {
+
+}
+
+uint GDIndex::get_number_global_descriptors() {
+    return index_.number_global_descriptors;
+}
+
+void GDIndex::generate_index(const vector<string>& feature_files, 
+                             const int verbose_level) {
+
+}
+
+void GDIndex::generate_index_shot_based(const vector<string>& feature_files, 
+                                        const vector<uint>& shot_beg_frames,
+                                        const int shot_mode, const int shot_keyf, 
+                                        const vector < vector < 
+                                            pair < uint, uint > > >& track_lists,
+                                        const int verbose_level) {
+
+}
+
+void GDIndex::generate_global_descriptor(const FeatureSet* feature_set, 
+                                         vector<uint>& gd_word_descriptor, 
+                                         vector<float>& gd_word_l1_norm, 
+                                         vector<float>& gd_word_total_soft_assignment) {
+
+}
+
+void GDIndex::performQuery(const string local_descriptors_path, 
+                           vector< pair<float,uint> >& results, 
+                           const vector<uint>& indices,
+                           const uint num_scenes_to_rerank,
+                           const uint group_testing_number_centroids ,
+                           const GDIndex* revv_other_ptr,
+                           const vector < vector < uint > >& vGroupLists,
+                           const vector < pair < string, pair < uint, uint > > >& shot_info,
+                           const int verbose_level) {
+
+}
+
+void GDIndex::set_index_parameters(const uint ld_length, const uint ld_frame_length,
+                                   const string ld_extension, const string ld_name,
+                                   const uint ld_pca_dim, const float ld_pre_pca_power,
+                                   const uint gd_number_gaussians, const float gd_power,
+                                   const string trained_parameters_path,
+                                   const int verbose_level) {
+
+}
+
+void GDIndex::set_query_parameters(const uint min_number_words_visited,
+                                   const int word_selection_mode,
+                                   const float word_selection_thresh,
+                                   const string trained_parameters_path,
+                                   const int verbose_level) {
+
+}
+
+void GDIndex::update_index() {
+
+}
+
+void GDIndex::sign_binarize(const vector<float>& gd_word_residuals, 
+                            vector<uint>& gd_word_descriptor) {
+
+}
+
+void GDIndex::project_local_descriptr_pca(const float* desc, float* pca_desc) {
+
+}
+
+void GDIndex::sampleFramesFromShot(const uint number_frames_out, 
+                                   const uint first_frame, 
+                                   const uint number_frames_this_shot, 
+                                   vector<uint>& out_frames) {
+
+}
+
+void GDIndex::query(const vector<uint>& query_word_descriptor,
+                    const vector<float>& query_word_l1_norm,
+                    const vector<float>& query_word_total_soft_assignment,
+                    const vector<uint>& database_indices,
+                    vector< pair<float,uint> >& database_scores_indices) {
+
+}
+
+void GDIndex::load_ld_mean_vector(string path) {
+
+}
+
+void GDIndex::load_ld_pca_eigenvectors(string path) {
+
+}
+
+void GDIndex::load_gd_gmm(string path) {
+
+}
+
+void GDIndex::load_corr_weights(string path) {
+
+}
+
---
+++ indexer/global_descriptors/gdindex.h
@@ -0,0 +1,220 @@
+#ifndef GDINDEX_H
+#define GDINDEX_H
+
+#include <string>
+#include <vector>
+
+#include "../../common/feature_set/feature_set.h"
+
+extern "C" {
+#include "../../common/yael_v438_modif/yael/gmm.h"
+}
+
+using namespace std;
+
+#ifndef PUSH_BIT
+#define PUSH_BIT(packed, bit) \
+  packed = packed << 1; \
+  packed += bit;
+#endif
+
+#ifndef POWER_LAW
+#define POWER_LAW(v, a) \
+  v = (v >= 0) ? pow(v,a) : -1 * pow(-v,a);
+#endif
+
+typedef unsigned char uchar;
+typedef unsigned int uint;
+typedef unsigned short ushort;
+
+// Default initializations for private variables
+const uint LD_LENGTH_DEFAULT = 128;
+const uint LD_FRAME_LENGTH_DEFAULT = 4;
+const string LD_EXTENSION_DEFAULT = ".siftb";
+const string LD_NAME_DEFAULT = "sift";
+const uint LD_PCA_DIM_DEFAULT = 32;
+const float LD_PRE_PCA_POWER_DEFAULT = 0.5;
+const uint GD_NUMBER_GAUSSIANS_DEFAULT = 512;
+const float GD_POWER_DEFAULT = 0.5;
+const uint MIN_NUMBER_WORDS_VISITED_DEFAULT = 20;
+const int WORD_SELECTION_MODE_DEFAULT = 0;
+const float WORD_SELECTION_THRESH_DEFAULT = 7;
+
+class GDIndex
+{
+ public:
+
+  GDIndex();
+
+  // Index I/O
+  void write(const string index_path);
+  void read(const string index_path);
+  // -- This function writes a file useful in the case of using shots 
+  //      with independent keyframes indexing
+  void write_frame_list(const string file_path);
+
+  // Clean variables in index_
+  void clean_index();
+
+  // Get number of stored global descriptors in index
+  uint get_number_global_descriptors();
+
+  // Generate index: these will populate index_
+  // -- frame-based signatures
+  void generate_index(const vector<string>& feature_files, 
+                      const int verbose_level = 1);
+  // -- shot/scene-based signatures
+  void generate_index_shot_based(const vector<string>& feature_files, 
+                                 const vector<uint>& shot_beg_frames,
+                                 const int shot_mode, const int shot_keyf, 
+                                 const vector < vector < 
+                                   pair < uint, uint > > >& track_lists,
+                                 const int verbose_level = 1);
+  // -- generate one global descriptor from features
+  void generate_global_descriptor(const FeatureSet* feature_set, 
+                                  vector<uint>& gd_word_descriptor, 
+                                  vector<float>& gd_word_l1_norm, 
+                                  vector<float>& gd_word_total_soft_assignment);
+
+  // Query index from query's local descriptor path
+  void performQuery(const string local_descriptors_path, 
+                    vector< pair<float,uint> >& results, 
+                    const vector<uint>& indices = vector<uint>(),
+                    const uint num_scenes_to_rerank = 0,
+                    const uint group_testing_number_centroids = 0,
+                    const GDIndex* revv_other_ptr = NULL,
+                    const vector < vector < uint > >& vGroupLists 
+                      = vector < vector < uint > >(), 
+                    const vector < pair < string, pair < uint, uint > > >& shot_info
+                      = vector < pair < string, pair < uint, uint > > >(),
+                    const int verbose_level = 1);
+
+  // Function to set index_parameters_
+  void set_index_parameters(const uint ld_length, const uint ld_frame_length,
+                            const string ld_extension, const string ld_name,
+                            const uint ld_pca_dim, const float ld_pre_pca_power,
+                            const uint gd_number_gaussians, const float gd_power,
+                            const string trained_parameters_path,
+                            const int verbose_level = 1);
+
+  // Function to set query_parameters_
+  // -- Note that the correlation weights loaded here require that
+  //    some of the index_parameters_ variables be set. So, ALWAYS
+  //    load index_parameters_ BEFORE loading query_parameters_
+  void set_query_parameters(const uint min_number_words_visited,
+                            const int word_selection_mode,
+                            const float word_selection_thresh,
+                            const string trained_parameters_path,
+                            const int verbose_level = 1);
+ private:
+  /************ Constants *************/
+  // Modes for shot detection
+  enum {SHOT_MODE_INDEP_KEYF = 0, SHOT_MODE_SHOT_AGG = 1, SHOT_MODE_GLOBAL_AGG = 2, SHOT_MODE_TRACK_AGG = 3};
+  // Mode used in extra word information (extra_word_info_mode)
+  enum {EXTRA_WORD_INFO_L1_NORM = 0, EXTRA_WORD_INFO_SOFT_ASSGN = 1, EXTRA_WORD_INFO_BOTH = 2};
+  /************ End of Constants *************/
+
+  /************ Variable  *************/  
+  // Variables that will hold the index and number of signatures stored
+  struct struct_index {
+      vector < vector < uint > > word_descriptor;
+      vector < vector < float > > word_l1_norms;
+      vector < vector < float > > word_total_soft_assignment;
+      uint number_global_descriptors;
+      // Vector that keeps frame numbers that are actually indexed in the db.
+      // This is used basically when using shots with indep. keyf. indexing,
+      // ie, SHOT_MODE_INDEP_KEYF mode
+      vector<uint> frame_numbers_in_db;
+  };
+  struct_index index_;
+
+  // Index parameters
+  struct struct_index_parameters {
+      // Local descriptor information
+      uint ld_length;
+      uint ld_frame_length;
+      string ld_extension;
+      string ld_name;
+
+      // Parameters for PCA-ing local descriptors
+      uint ld_pca_dim;
+      float ld_pre_pca_power;
+      float* ld_mean_vector;
+      vector<float*> ld_pca_eigenvectors;      
+
+      // Parameters used for global descriptor computation
+      gmm_t* gd_gmm;
+      uint gd_number_gaussians;
+      float gd_power;
+  };
+  struct_index_parameters index_parameters_;
+
+  // Variables relevant for query time
+  struct struct_query_parameters {
+      // -- Number of minimum words to require for matching
+      uint min_number_words_visited;
+      // -- Type of extra word information currently in use
+      // Note: this is only used if use_extra_word_info is set to true
+      // EXTRA_WORD_INFO_L1_NORM: only globalWordL1Norm is used
+      // EXTRA_WORD_INFO_SOFT_ASSGN: only globalWordTotalSoftAssignment is used
+      // EXTRA_WORD_INFO_BOTH: both globalWordL1Norm and globalWordTotalSoftAssignment
+      //                       are used (default)
+      int word_selection_mode;
+      // -- Threshold to use in visual word selection (used in asymmetric mode)
+      float word_selection_thresh;
+      // -- Parameters used in scoring
+      float* fast_corr_weights;
+      int pop_count[65536];
+  };
+  struct_query_parameters query_parameters_;
+
+  /************ End of Variables  *************/
+
+  /************ Functions *************/
+  // Update index after additions or deletions to it
+  void update_index();
+
+  // Sign binarize residuals
+  void sign_binarize(const vector<float>& gd_word_residuals, 
+                     vector<uint>& gd_word_descriptor);
+
+  // PCA projection for local descriptors
+  void project_local_descriptr_pca(const float* desc, float* pca_desc);
+
+  // Helper function to compare pairs
+  static bool cmp_float_uint_ascend(const pair<float,uint> pair1, 
+                                    const pair<float,uint> pair2) {
+      return pair1.first < pair2.first;
+  }
+
+  // This function samples the number_frames_out from the shot that begins at
+  // frame first_frame and contains number_frames_this_shot, and returns the frame
+  // indices in a sorted vector.
+  // If one frame is requested (number_frames_out = 1), it will return the center one;
+  // otherwise, it will try to take equally spaced frames; if this results in frames 
+  // too concentrated at the beginning or the end, it will take only the center ones.
+  void sampleFramesFromShot(const uint number_frames_out, 
+                            const uint first_frame, 
+                            const uint number_frames_this_shot, 
+                            vector<uint>& out_frames);
+
+  // Query index with query global descriptor
+  void query(const vector<uint>& query_word_descriptor,
+             const vector<float>& query_word_l1_norm,
+             const vector<float>& query_word_total_soft_assignment,
+             const vector<uint>& database_indices,
+             vector< pair<float,uint> >& database_scores_indices);
+
+  // Functions to load trained parameters from index_parameters_
+  void load_ld_mean_vector(string path);
+  void load_ld_pca_eigenvectors(string path);
+  void load_gd_gmm(string path);
+
+  // Functions to load trained parameters from query_parameters_
+  void load_corr_weights(string path);
+  /************ End of Functions *************/
+
+
+};
+
+#endif
