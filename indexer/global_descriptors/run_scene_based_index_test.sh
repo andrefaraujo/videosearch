cf6785c971af3896d7b34ab15dc774ff3cfa7dd8
Andre Araujo <andrefaraujo@gmail.com>
2015-10-04 22:49:14 -0700

    Scripts to extract shot and scene based global descriptors

---
+++ indexer/global_descriptors/run_scene_based_index_test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Get list of keyframe lists
+KEYFRAMES_LISTS=test_keyframe_lists.txt
+ls ../test_db/*.txt > $KEYFRAMES_LISTS
+
+# Parameters for index
+GDINDEX_PATH=trained_parameters
+CENTROIDS=512
+LD_MODE=0
+VERBOSE=1
+
+# Scene parameters
+SCENE_MODE=1
+SCENE_KEYF=-1
+
+# Loop over each video and get shot-based index
+# for each of them
+for list in `cat $KEYFRAMES_LISTS`; do
+
+    # Compose output index name
+    out_index=${list%.txt}.sift_scfv_idx_k${CENTROIDS}_scene_n${SCENE_KEYF}_m${SCENE_MODE}
+
+    # Command line
+    cmd=$(echo time \
+        ./index_dataset \
+	    -i $list \
+        -o $out_index \
+        -r $GDINDEX_PATH \
+        -c $CENTROIDS \
+        -l $LD_MODE \
+        -v $VERBOSE \
+        -m $SCENE_MODE \
+        -k $SCENE_KEYF \
+        --single_shot)
+
+    # Write and execute command
+    echo $cmd
+    $cmd
+done
