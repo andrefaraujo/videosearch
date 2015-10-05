be0751658c9d1b1c2125a3ad4c3326284c4ffb23
Andre Araujo <andrefaraujo@gmail.com>
2015-10-04 20:28:03 -0700

    Script to extract SIFT from test query image

---
+++ indexer/local_descriptors/run_sift_extraction_test_query.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get list of keyframes
+IMAGE_LIST=test_query_list.txt
+ls ../test_query/test_query.jpg > $IMAGE_LIST
+
+# Choose the number of threads
+NUMBER_THREADS=1
+
+./extract -t ${NUMBER_THREADS} -i ${IMAGE_LIST} -s
