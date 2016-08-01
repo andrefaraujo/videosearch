4500d47460649b32644b8c3e3d3d6671eb81600a
Andre Araujo <andrefaraujo@gmail.com>
2016-08-01 13:11:45 -0700

    Modified scoring scripts to get results using SIFTGeo descs

--- scoring/run_convert_frame_based_results_test_query_index.sh
+++ scoring/run_convert_frame_based_results_test_query_index.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Feature mode: 0=SIFT; 1=SIFTGeo
+FEAT_MODE=0
+if [ $FEAT_MODE -eq 0 ]; then
+    FEAT_NAME=sift
+elif [ $FEAT_MODE -eq 1 ]; then
+    FEAT_NAME=siftgeo
+else
+    echo "Unrecognized LD_NAME"
+    exit
+fi
+
 NUMBER_RESULTS=100
-RESULTS_FILE=../retriever/test_query_index/results/SCFV_frames/gaussians_512/ws_mode_0/ws_thresh_10/min_num_words_visited_0/out_results.txt
-RESULTS_SCENES_FILE=../retriever/test_query_index/results/SCFV_frames/gaussians_512/ws_mode_0/ws_thresh_10/min_num_words_visited_0/out_scene_results.txt
+RESULTS_FILE=../retriever/test_query_index/results/SCFV_frames/feat_name_${FEAT_NAME}/gaussians_512/ws_mode_0/ws_thresh_10/min_num_words_visited_0/out_results.txt
+RESULTS_SCENES_FILE=../retriever/test_query_index/results/SCFV_frames/feat_name_${FEAT_NAME}/gaussians_512/ws_mode_0/ws_thresh_10/min_num_words_visited_0/out_scene_results.txt
 
 # Compose command line
 cmd=$(echo \
