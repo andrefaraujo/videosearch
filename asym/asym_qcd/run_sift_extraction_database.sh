e0ee54ba2ba1810da845a7e937d4b32b5cf6a273
Andre Araujo <afaraujo@stanford.edu>
2016-09-06 16:17:50 -0700

    Removing asum old scripts

--- asym/asym_qcd/run_sift_extraction_database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# Get list of images
-IMAGE_LIST=db_list_clutter0005_public.txt
-
-# Choose the number of threads
-NUMBER_THREADS=10
-
-../../indexer/local_descriptors/extract -t ${NUMBER_THREADS} -i ${IMAGE_LIST} -s
